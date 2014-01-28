6182ee4ad94681c97fb5da368c173f9436523204
Luke <Luke@Luke-PC.home>
2014-01-28 17:52:42 +0000

    Started work on more skills. (Alistar - Headbutt)

---
+++ League of Minecraft/de/elicis/lom/champions/skills/Alistar/Headbutt.java
@@ -0,0 +1,19 @@
+package de.elicis.lom.champions.skills.Alistar;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import de.elicis.lom.champions.skills.Skill;
+
+public class Headbutt extends Skill{
+
+	public Headbutt(Player player2, int mana, ItemStack iconItem, int slot, int cooldown) {
+		super(player2, mana, iconItem, slot, cooldown);
+	}
+
+	@Override
+	public void useSkill() {
+		
+	}
+
+}
