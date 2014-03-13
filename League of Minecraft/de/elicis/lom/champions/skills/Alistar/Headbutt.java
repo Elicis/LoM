4781b8959718c2eee636f1e742bb42f483274bfb
Luke <Luke@Luke-PC>
2014-03-13 18:12:07 +0000

    Started more work on Alistar's headbutt

--- League of Minecraft/de/elicis/lom/champions/skills/Alistar/Headbutt.java
+++ League of Minecraft/de/elicis/lom/champions/skills/Alistar/Headbutt.java
@@ -1,7 +1,9 @@
 package de.elicis.lom.champions.skills.Alistar;
 
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import de.elicis.lom.champions.skills.Skill;
 
@@ -10,10 +12,16 @@ public class Headbutt extends Skill{
 	public Headbutt(Player player2, int mana, ItemStack iconItem, int slot, int cooldown) {
 		super(player2, mana, iconItem, slot, cooldown);
 	}
-
+	
+	public void setItemSlot(){
+		ItemMeta im = getIconItem().getItemMeta();
+		im.setDisplayName("Headbutt");
+		getIconItem().setItemMeta(im);
+		getPlayer().getInventory().setItem(getSlot(), getIconItem());
+	}
+	
 	@Override
 	public void useSkill() {
-		
+		getPlayer().playSound(getPlayer().getLocation(), Sound.BLAZE_DEATH, 10, 1);
 	}
-
 }
